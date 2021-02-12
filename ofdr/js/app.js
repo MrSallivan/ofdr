0cde341aa788ab012dc0d5f4c50ef4c72b1b3f1a
MrSallivan <nik.sallivan@gmail.com>
2021-02-12 22:33:40 +1000

    12.02

--- ofdr/js/app.js
+++ ofdr/js/app.js
@@ -5,7 +5,20 @@ let btn = document.querySelector('._load-more')
 btn.addEventListener('click', function (event) {
 	const btnclick = event.target
 	console.log(btnclick)
-	do
+	let xhttp = new XMLHttpRequest()
+
+	xhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			myFunction(this.responseText)
+		}
+	}
+	xhttp.open('GET', '../news.html', true)
+	xhttp.send()
+
+
+	function myFunction(data) {
+		console.log(data)
+	}
 })
 //BildSlider
 let sliders = document.querySelectorAll('._swiper');
