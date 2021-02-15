d575f3a87e8ae80f7699f6e2bd6bf4b310fb75b3
MrSallivan <nik.sallivan@gmail.com>
2021-02-15 23:04:43 +1000

    15.02.01

--- ofdr/js/app.js
+++ ofdr/js/app.js
@@ -1,27 +1,7 @@
 function email_test(input) {
 	return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
 }
-let btn = document.querySelector('._load-more')
 
-btn.addEventListener('click', function (event) {
-
-	let request = new XMLHttpRequest();
-	request.open('GET', 'news.html');
-
-	request.addEventListener('readystatechange', function () {
-
-		if (request.readyState === 4 && request.status === 200) {
-
-			let block = document.body.querySelector('.morenews')
-			let content = request.response
-			console.log(content)
-			block.innerHTML = content
-		}
-
-
-	})
-	request.send()
-})
 //BildSlider
 let sliders = document.querySelectorAll('._swiper');
 if (sliders) {
